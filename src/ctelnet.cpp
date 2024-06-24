48dce2e05f1593bb489e3ad66cd3b1fee40793d1
MTSowbug <mtsowbug@gmail.com>
2024-06-23 23:32:19 -0500

    Update ctelnet.cpp
    removed more cumbersome messages

--- src/ctelnet.cpp
+++ src/ctelnet.cpp
@@ -2025,9 +2025,9 @@ void cTelnet::setGMCPVariables(const QByteArray& msg)
             mpHost->mServerGUI_Package_version = version;
         }
 
-        postMessage(tr("[ INFO ]  - Server offers downloadable GUI (url='%1') (package='%2').").arg(url, packageName));
+        // postMessage(tr("[ INFO ]  - Server offers downloadable GUI (url='%1') (package='%2').").arg(url, packageName));
         if (mpHost->mInstalledPackages.contains(packageName)) {
-            postMessage(tr("[  OK  ]  - Package is already installed."));
+            // postMessage(tr("[  OK  ]  - Package is already installed."));
         } else {
             mServerPackage = mudlet::getMudletPath(mudlet::profileDataItemPath, mProfileName, fileName);
             mpHost->updateProxySettings(mpDownloader);
